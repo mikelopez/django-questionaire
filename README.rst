1dbdc84e81fc648955ac41a282800b0e3237f61d
Mike Lopez <dev@scidentify.info>
2013-09-22 03:20:22 -0400

    initial import / skel

---
+++ README.rst
@@ -0,0 +1,7 @@
+Readme for django-questionaire
+------------------------------
+
+Facilitates the process of creating a Questoinair (Or application) and recording users answers.
+The application can then be approved by an admin.
+
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='django-questionaire',
+      version='0.1',
+      description='Description for django-questionaire',
+      author='',
+      license='MIT',
+      py_modules=['django-questionaire'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Terminals'
+          ]
+      )
