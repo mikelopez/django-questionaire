7ac7799d6d082ec3365d49c184f1791683e3f250
Mike Lopez <dev@scidentify.info>
2013-10-07 19:45:46 -0400

    new import

--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import sys, os
 
-setup(name='django-questionaire',
-      version='0.1',
-      description='Description for django-questionaire',
-      author='',
-      license='MIT',
-      py_modules=['django-questionaire'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Terminals'
-          ]
-      )
+setup(
+    name='django-questionaire',
+    version='0.1',
+    description="django-questionaire Description",
+    long_description=open('README.md', 'r').read(),
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='MIT',
+    package_dir={'django-questionaire': 'django-questionaire'},
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: Log Analysis",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Page Counters",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Security",
+        "Topic :: System :: Monitoring",
+        "Topic :: Utilities",
+    ]
+)
+
+>>>>>>> new import
