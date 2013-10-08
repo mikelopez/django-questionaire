d760fa807cbac33ecaa239847c71e8700138cb97
Mike Lopez <dev@scidentify.info>
2013-10-07 20:14:01 -0400

    runner

---
+++ runtests.py
@@ -0,0 +1,21 @@
+import os, sys
+from django.conf import settings
+
+settings.configure(DEBUG=True,
+               DATABASES={
+                    'default': {
+                        'ENGINE': 'django.db.backends.sqlite3',
+                    }
+                },
+               ROOT_URLCONF='questionaire.urls',
+               INSTALLED_APPS=('django.contrib.auth',
+                              'django.contrib.contenttypes',
+                              'django.contrib.sessions',
+                              'django.contrib.admin',
+                              'questionaire',))
+
+from django.test.simple import DjangoTestSuiteRunner
+test_runner = DjangoTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(['questionaire', ])
+if failures:
+    sys.exit(failures)
