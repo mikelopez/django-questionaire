9042838873566f14d41dfcaf86f3ce532bf91318
Mike Lopez <dev@scidentify.info>
2013-10-07 21:59:51 -0400

    base tests

--- questionaire/tests.py
+++ questionaire/tests.py
@@ -7,10 +7,40 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+class BaseTest(TestCase):
+	"""
+	Base Test.
+	"""
+	def setUp(self):
+		pass
+	def create_questionaire(self, **kwargs):
+		"""
+		Creates a Questionaire Object.
+		"""
+		q = Questionaire(**kwargs)
+		q.save()
+		return q
+
+	def add_question(self, **kwargs):
+		"""
+		Add a Question to a Questionaire
+		"""
+		q = Question(**kwargs)
+		q.save()
+		return q
+
+	def write_answer(self, **kwargs):
+		"""
+		Write an answer to a question.
+		"""
+		a = Answer(**kwargs)
+		a.save()
+		return a
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+    	pass
+
+    def test_questionaire(self):
+    	self.assertTrue(self.create_questionaire("EmployeeSurvey"))
+
