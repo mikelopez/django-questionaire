7a2bb02fbdc713c7f06ee50edd625ded601dca38
Mike Lopez <dev@scidentify.info>
2013-10-11 03:58:50 -0400

    check for types in question-types

--- questionaire/tests/test_questionaire.py
+++ questionaire/tests/test_questionaire.py
@@ -20,6 +20,13 @@ class QuestionaireTestCase(BaseTest):
     def setUp(self):
         pass
 
+    def test_question_types(self):
+        """Checks for the right question types."""
+        types = Question.TYPES
+        for val in ['yes_or_no', 'multiple_choice', 'text']:
+            self.assertTrue([t for t in types if val in ])
+
+
     def test_questionaire(self):
         """Creates a new Questionaire."""
         self.assertTrue(self.create_questionaire(name="EmployeeSurvey"))
@@ -28,14 +35,14 @@ class QuestionaireTestCase(BaseTest):
         """Creates some questions on the questionaire."""
         questionaire = self.create_questionaire(name="ABC")
         question = questionaire.add_question(question="Do you like the color blue?", 
-                                          parent=questionaire)
+                                             parent=questionaire)
         self.assertEquals(questionaire.questions.count(), 1)
 
     def test_add_answer(self):
         """Creates an answer for the questions on a questionaire."""
         questionaire = self.create_questionaire(name="UserSurvey")
         questionaire.save()
-        question = self.add_question(question="Do you like fishsticks?", 
+        question = questionaire.add_question(question="Do you like fishsticks?", 
                                      parent=questionaire)
         question.save()
         self.assertTrue(self.write_answer(answer="Sometimes, I do", 
